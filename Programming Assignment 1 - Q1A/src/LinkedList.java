9e87f29661dcd014096c38a327a3635516ce4c7d
SKT1803 <skemalt@outlook.com>
2024-10-04 16:05:17 +0300

    Update LinkedList.java

--- Programming Assignment 1 - Q1A/src/LinkedList.java
+++ Programming Assignment 1 - Q1A/src/LinkedList.java
@@ -1,5 +1,6 @@
 //-----------------------------------------------------
 // Title: LinkedList class
+// Author: Serdar Kemal Topkaya
 // Description: In this class, a matrix taken as a parameter is translated and displayed 
 // in a spiral format LinkedList with the help of add, matrixToSpiralList and toString methods.
 //-----------------------------------------------------
