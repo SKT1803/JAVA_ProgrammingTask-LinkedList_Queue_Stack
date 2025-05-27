bf5c70c87d89cca6edcfd2e5e187381c187f41da
SKT1803 <skemalt@outlook.com>
2025-05-27 20:26:52 +0300

    Update Main.java

--- Programming Assignment 1 - Q2/src/Main.java
+++ Programming Assignment 1 - Q2/src/Main.java
@@ -1,5 +1,5 @@
 // Title: Main class
-// Author: Serdar Kemal Topkaya
+// Author: SKT
 // Description: This class takes bitcoin values from text files as String values 
 // and converts them to integer values. By calling the methods in the Queue class, 
 // it outputs the required bitcoin values and the range of these values.
